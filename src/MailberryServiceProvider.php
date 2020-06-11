05498d663e5a9178ba6a241aac85afde7f3876d7
Artem <artem.fufaldin@gmail.com>
2020-06-11 23:07:27 +0500

    add config publisher

--- src/MailberryServiceProvider.php
+++ src/MailberryServiceProvider.php
@@ -7,18 +7,36 @@ use Illuminate\Support\ServiceProvider;
 class MailberryServiceProvider extends ServiceProvider
 {
     /**
+     * Адресат для получения всех писем вне production.
+     *
+     * @var array
+     */
+    protected $to;
+
+    /**
      * Register services.
+     *
+     * @return void
      */
     public function register()
     {
-        //
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/mailberry.php',
+            'mailberry'
+        );
+
+        $this->to = config('mailberry.to');
     }
 
     /**
      * Bootstrap services.
+     *
+     * @return void
      */
     public function boot()
     {
-        //
+        $this->publishes([
+            __DIR__.'/../config/mailberry.php' => config_path('mailberry.php'),
+        ]);
     }
 }
